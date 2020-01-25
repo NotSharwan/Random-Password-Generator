db39229cb20ab81b06c32200f16f168c9d3f7fcb
Sharwan Solanki <48463223+NotSharwan@users.noreply.github.com>
2020-01-25 11:47:21 +0530

    Add files via upload

---
+++ RandomPasswordGenerator.py
@@ -0,0 +1,16 @@
+import random
+alphabets= "abcdefghijklmnopqrstuvwxyz"
+numbers="1234567890"
+special="!@#$%&*"
+a=int(input("Enter the Number of Alphabets:"))
+n=int(input("Enter the Number of Digits:"))
+s=int(input("Enter the Number of Symbols:"))
+password = ''
+for _ in range(a):
+    password += random.choice(alphabets)
+for _ in range(n):
+    password += random.choice(numbers)
+for _ in range(s):
+    password += random.choice(special)
+password=''.join(random.sample(password,len(password)))
+print("Generated Password:",password)
